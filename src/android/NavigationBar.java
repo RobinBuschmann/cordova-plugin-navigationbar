6701ca0a15d7e38d8b744dac7d7f94f9b668b999
Robin Buschmann <cm@Robins-MacBook-Pro.local>
2016-02-17 16:32:05 +0100

    updated fullscreen tags (the prev combination caused the statusbar to show)

--- src/android/NavigationBar.java
+++ src/android/NavigationBar.java
@@ -140,8 +140,10 @@ public class NavigationBar extends CordovaPlugin {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private void _hideNavigationBar(Activity activity){
         activity.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
     }
